001fe776ebc456729a42ee7341239dc871f8bb62
Kyle Andrews <kylea@page.works>
2019-04-05 06:28:56 -0400

    Adds inline documentation to the App class

--- app/typescript/app.ts
+++ app/typescript/app.ts
@@ -3,26 +3,32 @@ export default class App{
     private _buttons:   Array<HTMLButtonElement>;
 
     constructor(){
-        this._buttons   = Array.from(document.body.querySelectorAll('.js-button'));
 
-        for(let button of this._buttons){
-            button.addEventListener('click', this.handleLoad);
-        }
+        // Build an array of all the buttons
+        this._buttons   = Array.from(document.body.querySelectorAll('.js-button'));
         
+        // Call `init()` to start any event listeners
         this.init();
     }
 
+    /**
+     * Called when an element with the class of `.js-button` is clicked.
+     * Gets the component ID for the dynamic importer method.
+     */
     private handleLoad:EventListener = (e:Event)=>{
         const target = <HTMLElement>e.currentTarget;
         const component = target.dataset.componentId;
-        console.log(component);
     }
 
     /**
-     * Called when the class has be initiated
+     * Called when the class has be initiated.
+     * Used to assign event listeners to DOM elements.
      */
     private init():void{
-        
+        // Assign the `click` event listener to each of the buttons
+        for(let button of this._buttons){
+            button.addEventListener('click', this.handleLoad);
+        }
     }
 }
 
--- docs/assets/app.js
+++ docs/assets/app.js
@@ -2,21 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class App {
     constructor() {
+        /**
+         * Called when an element with the class of `.js-button` is clicked.
+         * Gets the component ID for the dynamic importer method.
+         */
         this.handleLoad = (e) => {
             const target = e.currentTarget;
             const component = target.dataset.componentId;
-            console.log(component);
         };
+        // Build an array of all the buttons
         this._buttons = Array.from(document.body.querySelectorAll('.js-button'));
-        for (let button of this._buttons) {
-            button.addEventListener('click', this.handleLoad);
-        }
+        // Call `init()` to start any event listeners
         this.init();
     }
     /**
-     * Called when the class has be initiated
+     * Called when the class has be initiated.
+     * Used to assign event listeners to DOM elements.
      */
     init() {
+        // Assign the `click` event listener to each of the buttons
+        for (let button of this._buttons) {
+            button.addEventListener('click', this.handleLoad);
+        }
     }
 }
 exports.default = App;
